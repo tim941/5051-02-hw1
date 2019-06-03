a0bff13401ac52b160887d306311a2567aff4664
Zuyan He <timmacy941@gmail.com>
2019-06-03 00:34:40 -0400

    Added ReportViewModel LogViewModel Get Test

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -46,5 +46,18 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual(3, result);
         }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+
+            // Act
+            var result = myTest.LogViewModel;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
