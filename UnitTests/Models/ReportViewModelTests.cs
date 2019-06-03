86f39bc0ae2633154d02e7ff56a4042bcddfaf6c
Zuyan He <timmacy941@gmail.com>
2019-06-03 00:22:14 -0400

    Added ReportViewModelTests.cs to the project

---
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            
+        }
+    }
+}
